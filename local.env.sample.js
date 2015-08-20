aeceac37ac72cb59f02c48cd02e3b4ad106a257a
Kla Haeck <kla.haeck@gmail.com>
2015-08-20 10:40:22 -0500

    added sample local env file

---
+++ local.env.sample.js
@@ -0,0 +1,8 @@
+// COPY THIS FILE AS "local.env.js" AND ADD YOUR TWIITER TOKENS TO TST LOCALLY
+
+module.exports = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+};
